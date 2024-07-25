8e067d439275d06a09cea695ea4d976ef61a0e7d
Sulstice <sharifsuliman1@gmail.com>
2024-07-25 15:55:14 -0500

    SS-1: Added the raw phrase as a prefix

--- stages/01_download_data.sh
+++ stages/01_download_data.sh
@@ -3,15 +3,15 @@
 mkdir -p ./download
 
 # PFAM Download -- Optional --
-wget -O download/pfam.tar.gz http://s3.amazonaws.com/songlabdata/proteindata/data_raw_pytorch/pfam.tar.gz
+wget -O download/raw_pfam.tar.gz http://s3.amazonaws.com/songlabdata/proteindata/data_raw_pytorch/pfam.tar.gz
 
 # Download Vocab/Model files
-wget -O download/pfam.model http://s3.amazonaws.com/songlabdata/proteindata/data_pytorch/pfam.model
-wget -O download/pfam.vocab http://s3.amazonaws.com/songlabdata/proteindata/data_pytorch/pfam.vocab
+wget -O download/raw_pfam.model http://s3.amazonaws.com/songlabdata/proteindata/data_pytorch/pfam.model
+wget -O download/raw_pfam.vocab http://s3.amazonaws.com/songlabdata/proteindata/data_pytorch/pfam.vocab
 
 # Download Data Files
-wget -O download/secondary_structure.tar.gz http://s3.amazonaws.com/songlabdata/proteindata/data_raw_pytorch/secondary_structure.tar.gz
-wget -O download/proteinnet.tar.gz http://s3.amazonaws.com/songlabdata/proteindata/data_raw_pytorch/secondary_structure.tar.gz
-wget -O download/remote_homology.tar.gz http://s3.amazonaws.com/songlabdata/proteindata/data_raw_pytorch/remote_homology.tar.gz
-wget -O download/fluorescence.tar.gz http://s3.amazonaws.com/songlabdata/proteindata/data_raw_pytorch/fluorescence.tar.gz
-wget -O download/stability.tar.gz http://s3.amazonaws.com/songlabdata/proteindata/data_raw_pytorch/stability.tar.gz
+wget -O download/raw_secondary_structure.tar.gz http://s3.amazonaws.com/songlabdata/proteindata/data_raw_pytorch/secondary_structure.tar.gz
+wget -O download/raw_proteinnet.tar.gz http://s3.amazonaws.com/songlabdata/proteindata/data_raw_pytorch/secondary_structure.tar.gz
+wget -O download/raw_remote_homology.tar.gz http://s3.amazonaws.com/songlabdata/proteindata/data_raw_pytorch/remote_homology.tar.gz
+wget -O download/raw_fluorescence.tar.gz http://s3.amazonaws.com/songlabdata/proteindata/data_raw_pytorch/fluorescence.tar.gz
+wget -O download/raw_stability.tar.gz http://s3.amazonaws.com/songlabdata/proteindata/data_raw_pytorch/stability.tar.gz
--- stages/02_unzip_data.sh
+++ stages/02_unzip_data.sh
@@ -2,11 +2,11 @@
 
 mkdir -p ./extract
 
-tar -xvf download/pfam.tar.gz -C extract/
+tar -xvf download/raw_pfam.tar.gz -C extract/
 
-tar -xvf  download/secondary_structure.tar.gz -C extract/
-tar -xvf  download/proteinnet.tar.gz -C extract/
-tar -xvf  download/remote_homology.tar.gz -C extract/
-tar -xvf  download/fluorescence.tar.gz -C extract/
-tar -xvf  download/stability.tar.gz -C extract/
+tar -xvf  download/raw_secondary_structure.tar.gz -C extract/
+tar -xvf  download/raw_proteinnet.tar.gz -C extract/
+tar -xvf  download/raw_remote_homology.tar.gz -C extract/
+tar -xvf  download/raw_fluorescence.tar.gz -C extract/
+tar -xvf  download/raw_stability.tar.gz -C extract/
 
